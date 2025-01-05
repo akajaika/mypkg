f6c86697b251707c09f00ea99608919719910b8b
akajaika <akajaika@icloud.com>
2025-01-05 18:08:03 +0900

    [fix] test

--- test/test.bash
+++ test/test.bash
@@ -6,10 +6,10 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc 
-# timeout 10 ros2 run mypkg test_listener > /tmp/mypkg.log
+timeout 10 ros2 run mypkg test_listener > /tmp/mypkg.log
 cd $dir/ros2_ws/src/mypkg/mypkg/
 timeout 10 python3 similality_images.py
 
-# cat /tmp/mypkg.log |
-# grep 'Received similarity data: array'
-# grep 'Received filenames'
+cat /tmp/mypkg.log |
+grep 'Received similarity data: array'
+grep 'Received filenames'
